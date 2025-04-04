f3d399b9020a95a42335cd2f3cecac12ec3b3d8a
isank28 <sanketp7250@gmail.com>
2025-04-04 16:40:06 -0700

    Create inclassexcercises.sv

---
+++ inclassexcercises.sv
@@ -0,0 +1,19 @@
+// Code your testbench here
+// or browse Examples
+// module top;
+//   always @ (*) begin
+//   $display(9.0** (1/2));
+//   end
+// endmodule
+// module top;
+//   always @ (*) begin
+//     $display(9.0 ** (1.0 / 2.0)); // Perform floating-point division
+//   end
+// endmodule
+module top;
+  initial begin
+    $display(-13 % 4); // Calculate and display square root
+  end
+endmodule
+
+
